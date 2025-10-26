3de1d4d1b39708523f3c6a291524102bd9d84eb6
Hanh <hanh425@gmail.com>
2025-10-27 07:59:10 +0800

    feat(builder): locate nullifier in ranges

--- zcash-vote2/src/error.rs
+++ zcash-vote2/src/error.rs
@@ -2,6 +2,8 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
+    #[error("Duplicate Nullifier")]
+    DuplicateNullifier,
     #[error(transparent)]
     DbError(#[from] sqlx::Error),
     #[error(transparent)]
