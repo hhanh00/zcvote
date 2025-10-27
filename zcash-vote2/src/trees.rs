29573cf613a9ca240b5c95855080718a01feb023
hhanh00 <hanh425@gmail.com>
2025-10-27 08:24:52 +0800

    feat(builder): locate nullifier in ranges (#22)

--- zcash-vote2/src/trees.rs
+++ zcash-vote2/src/trees.rs
@@ -1,8 +1,9 @@
 use halo2_proofs::pasta::Fp;
 use orchard::vote::OrchardHash;
 use sqlx::SqliteConnection;
+use num_integer::Integer;
 
-use crate::{ProgressReporter, VoteResult};
+use crate::{ProgressReporter, VoteError, VoteResult};
 
 pub type AuthPath = Vec<OrchardHash>;
 
@@ -17,10 +18,31 @@ pub fn make_nfs_ranges(nfs: &mut Vec<Fp>) {
     *nfs.last_mut().unwrap() = Fp::one() - Fp::one(); // max
 }
 
+// Given a list of nullifiers and a sorted list of ranges
+// flatten as [a_i, b_i]
+// always return the lower bound a_i, positions must be even
+pub fn locate_nullifiers(nfs: &[Fp], ranges: &[Fp]) -> VoteResult<Vec<u32>> {
+    let mut poss = vec![];
+    for nf in nfs {
+        let pos = match ranges.binary_search(nf) {
+            Ok(p) => p.prev_multiple_of(&2),
+            Err(p) => {
+                if p.is_multiple_of(2) {
+                    // fallen between ranges, nf is not in any range
+                    return Err(VoteError::DuplicateNullifier);
+                }
+                p - 1
+            }
+        };
+        poss.push(pos as u32);
+    }
+    Ok(poss)
+}
+
 pub async fn compute_merkle_tree<PR: ProgressReporter>(
     connection: &mut SqliteConnection,
     query: String,
-    paths_req: Vec<Fp>,
+    pos_reqs: Vec<u32>,
     progress_reporter: PR,
 ) -> VoteResult<(OrchardHash, Vec<AuthPath>)> {
     Ok((OrchardHash([0u8; 32]), vec![]))
