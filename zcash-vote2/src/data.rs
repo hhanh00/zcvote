af596b4accf0030f033976768f0eb9847150b1a4
hhanh00 <hanh425@gmail.com>
2025-10-28 15:43:05 +0800

    chore: add test (#30)

--- zcash-vote2/src/data.rs
+++ zcash-vote2/src/data.rs
@@ -13,14 +13,14 @@ use crate::{
     trees::{compute_merkle_tree, make_nfs_ranges, orchard_hash},
 };
 
-#[derive(Clone, Serialize, Deserialize, Debug)]
+#[derive(Clone, Serialize, Deserialize, PartialEq, Debug)]
 pub struct CandidateChoice {
     pub address: Option<String>,
     pub choice: String,
 }
 
 /// Details of an election, including metadata, candidates, and election parameters.
-#[derive(Clone, Serialize, Deserialize, Default, Debug)]
+#[derive(Clone, Serialize, Deserialize, Default, PartialEq, Debug)]
 pub struct Election {
     pub name: String,
     #[serde(skip)]
@@ -34,7 +34,7 @@ pub struct Election {
     pub cmx_frontier: Option<Frontier>,
 }
 
-#[derive(Clone, Serialize, Deserialize, Default, Debug)]
+#[derive(Clone, Serialize, Deserialize, Default, PartialEq, Debug)]
 pub struct Question {
     pub question: String,
     pub choices: Vec<CandidateChoice>,
@@ -144,10 +144,8 @@ pub fn derive_vote_key(seed: &str, i: u32, j: u32) -> VoteResult<SpendingKey> {
 #[cfg(test)]
 mod tests {
     use anyhow::Result;
-    use sqlx::{
-        SqlitePool,
-        sqlite::SqliteConnectOptions,
-    };
+    use orchard::vote::OrchardHash;
+    use sqlx::{SqlitePool, sqlite::SqliteConnectOptions};
     use tokio::sync::mpsc;
 
     use crate::data::Election;
@@ -163,14 +161,70 @@ mod tests {
                 .await?;
         let election: Election = serde_json::from_str(&data).unwrap();
         let (tx, mut rx) = mpsc::channel::<String>(1);
-        let task = tokio::spawn(async move {
-            election.finalize(&mut *connection, tx).await.unwrap()
-        });
+        let task =
+            tokio::spawn(async move { election.finalize(&mut *connection, tx).await.unwrap() });
         while let Some(message) = rx.recv().await {
             println!("{message}");
         }
-        let e = task.await?;
-        println!("Finalized Election: {}", serde_json::to_string_pretty(&e).unwrap());
+        let mut e = task.await?;
+        println!(
+            "Finalized Election: {}",
+            serde_json::to_string_pretty(&e).unwrap()
+        );
+
+        e.questions.clear();
+        let expected = serde_json::from_str::<Election>(
+            r#"
+{
+  "name": "jaja",
+  "start_height": 2200000,
+  "end_height": 2200100,
+  "questions": [],
+  "signature_required": false,
+  "cmx": "b870f8e006bbb08dd5ff688386c4fba52148aa52840d857341d476751bcec835",
+  "nf": "c9d8599cbd375bbcfa43d79b31813120eaae210baf6fc3570af25ab1e5245912",
+  "cmx_frontier": {
+    "position": 99,
+    "leaf": "bb8fe205919617d739f08b1d5b0f036fb1669b68d41fbd4d411e93416933c004",
+    "ommers": [
+      "9a46a6bd7eeadbdf463fc6e6c53baa19df770aa1de82e135139e09c67640ad0f",
+      "087d2e5053515e7c8d7be186213f13ce8368eb1247d484d5c7107cbb4f45441f",
+      "c7413f4614cd64043abbab7cc1095c9bb104231cea89e2c3e0df83769556d030",
+      "2111fc397753e5fd50ec74816df27d6ada7ed2a9ac3816aab2573c8fac794204",
+      "806afbfeb45c64d4f2384c51eff30764b84599ae56a7ab3d4a46d9ce3aeab431",
+      "c8097eb66364f6b23fe1423dae87d8c66dfe9227daa64b978f2f269e136d790f",
+      "9c147ec100822a802b52f6ce75d75350b3658c97ccab8bfbd6e665e8f740d728",
+      "4e14563df191a2a65b4b37113b5230680555051b22d74a8e1f1d706f90f3133b",
+      "b3bbe4f993d18a0f4eb7f4174b1d8555ce3396855d04676f1ce4f06dda07371f",
+      "4ef5bde9c6f0d76aeb9e27e93fba28c679dfcb991cbcb8395a2b57924cbd170e",
+      "a3c02568acebf5ca1ec30d6a7d7cd217a47d6a1b8311bf9462a5f939c6b74307",
+      "3ef9b30bae6122da1605bad6ec5d49b41d4d40caa96c1cf6302b66c5d2d10d39",
+      "22ae2800cb93abe63b70c172de70362d9830e53800398884a7a64ff68ed99e0b",
+      "187110d92672c24cedb0979cdfc917a6053b310d145c031c7292bb1d65b7661b",
+      "3f98adbe364f148b0cc2042cafc6be1166fae39090ab4b354bfb6217b964453b",
+      "63f8dbd10df936f1734973e0b3bd25f4ed440566c923085903f696bc6347ec0f",
+      "2182163eac4061885a313568148dfae564e478066dcbe389a0ddb1ecb7f5dc34",
+      "bd9dc0681918a3f3f9cd1f9e06aa1ad68927da63acc13b92a2578b2738a6d331",
+      "ca2ced953b7fb95e3ba986333da9e69cd355223c929731094b6c2174c7638d2e",
+      "55354b96b56f9e45aae1e0094d71ee248dabf668117778bdc3c19ca5331a4e1a",
+      "7097b04c2aa045a0deffcaca41c5ac92e694466578f5909e72bb78d33310f705",
+      "e81d6821ff813bd410867a3f22e8e5cb7ac5599a610af5c354eb392877362e01",
+      "157de8567f7c4996b8c4fdc94938fd808c3b2a5ccb79d1a63858adaa9a6dd824",
+      "fe1fce51cd6120c12c124695c4f98b275918fceae6eb209873ed73fe73775d0b",
+      "1f91982912012669f74d0cfa1030ff37b152324e5b8346b3335a0aaeb63a0a2d",
+      "5dec15f52af17da3931396183cbbbfbea7ed950714540aec06c645c754975522",
+      "e8ae2ad91d463bab75ee941d33cc5817b613c63cda943a4c07f600591b088a25",
+      "d53fdee371cef596766823f4a518a583b1158243afe89700f0da76da46d0060f",
+      "15d2444cefe7914c9a61e829c730eceb216288fee825f6b3b6298f6f6b6bd62e",
+      "4c57a617a0aa10ea7a83aa6b6b0ed685b6a3d9e5b8fd14f56cdc18021b12253f",
+      "3fd4915c19bd831a7920be55d969b2ac23359e2559da77de2373f06ca014ba27",
+      "87d063cd07ee4944222b7762840eb94c688bec743fa8bdf7715c8fe29f104c2a"
+    ]
+  }
+}
+        "#,
+        )?;
+        assert_eq!(e, expected);
         Ok(())
     }
 }
